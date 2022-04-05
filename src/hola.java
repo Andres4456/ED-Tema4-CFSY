e2f19eecddc092e41c591353931f79e6983ed93a
Andres4456 <91089504+Andres4456@users.noreply.github.com>
2022-04-05 20:20:03 +0200

    Update hola.java

--- src/hola.java
+++ src/hola.java
@@ -1,6 +1,7 @@
 public class hola {
     public static void main(String[] args) {
         System.out.println("hola");
-        System.out.println("hola2 ");
+        System.out.println("hola 2");
+        System.out.println("hola 3")
     }
 }
