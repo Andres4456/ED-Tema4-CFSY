7f7ad95eb230aa4254ea6d160ae4648020e4b25f
Andres4456 <91089504+Andres4456@users.noreply.github.com>
2022-04-07 15:18:27 +0200

    Update hola.java

--- src/hola.java
+++ src/hola.java
@@ -2,6 +2,6 @@ public class hola {
     public static void main(String[] args) {
         System.out.println("hola");
         System.out.println("hola 2");
-        System.out.println("hola 3");
+        System.out.println("conflicto");
     }
 }
